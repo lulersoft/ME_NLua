9005c723e3cdbf11ff0d53c70ed6461b6063eced
lulersoft <2604904@qq.com>
2015-02-26 11:02:22 +0800

    扩展功能

--- Assets/Engine/Script/Common/API.cs
+++ Assets/Engine/Script/Common/API.cs
@@ -170,4 +170,75 @@ public class API  {
             timer.Dispose();
         }
     }
+
+
+    /// <summary>
+    /// HashToMD5Hex
+    /// </summary>
+    public static string HashToMD5Hex(string sourceStr)
+    {
+        byte[] Bytes = Encoding.UTF8.GetBytes(sourceStr);
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] result = md5.ComputeHash(Bytes);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+                builder.Append(result[i].ToString("x2"));
+            return builder.ToString();
+        }
+    }
+
+    /// <summary>
+    /// 计算字符串的MD5值
+    /// </summary>
+    public static string MD5(string source)
+    {
+        MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+        byte[] data = System.Text.Encoding.UTF8.GetBytes(source);
+        byte[] md5Data = md5.ComputeHash(data, 0, data.Length);
+        md5.Clear();
+
+        string destString = "";
+        for (int i = 0; i < md5Data.Length; i++)
+        {
+            destString += System.Convert.ToString(md5Data[i], 16).PadLeft(2, '0');
+        }
+        destString = destString.PadLeft(32, '0');
+        return destString;
+    }
+
+    /// <summary>
+    /// 计算文件的MD5值
+    /// </summary>
+    public static string MD5File(string file)
+    {
+        try
+        {
+            FileStream fs = new FileStream(file, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(fs);
+            fs.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("md5file() fail, error:" + ex.Message);
+        }
+    }
+
+    //发射线
+    public static object Raycast(Ray ray, out RaycastHit hit)
+    {
+        return Physics.Raycast(ray, out hit);
+    }
+    public static object Raycast(Ray ray, out RaycastHit hit, float distance, int layerMask)
+    {
+        return Physics.Raycast(ray, out hit, distance, layerMask);
+    }
 }
